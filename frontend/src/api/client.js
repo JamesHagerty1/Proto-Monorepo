a2fd1579ae7fdbd5613ad5adddcf66466c72b744
JamesHagerty1 <unkhonors@gmail.com>
2026-05-15 23:41:48 -0400

    frontend api client with credentials true

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: "",
   headers: { Accept: "application/json" },
+  withCredentials: true, // Allows the browser to attach cookies to requests
   timeout: 5000,
 });
