7ad821dcb21ef6dee2dffff52675146c2a59b343
JamesHagerty1 <unkhonors@gmail.com>
2026-05-17 17:45:28 -0400

    comment about devise User registerable

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Notice: `:registerable` is stripped away, because this app defines its
+  # signup flow
   devise :database_authenticatable, :recoverable, :rememberable, :validatable,
          :confirmable
 
